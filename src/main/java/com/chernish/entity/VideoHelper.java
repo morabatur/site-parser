cfc2fa92e295b246a27a9085239079be91efcf1e
Roman Chernish <morabatur@gmail.com>
2018-10-14 16:11:03 +0300

    change structure of class

--- src/main/java/com/chernish/entity/VideoHelper.java
+++ src/main/java/com/chernish/entity/VideoHelper.java
@@ -1,19 +1,28 @@
 package com.chernish.entity;
 
-import java.util.LinkedList;
+import com.chernish.parser.FolderParser;
+import com.chernish.parser.HTMLParser;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VideoHelper {
-    private LinkedList<Video> videoList = new LinkedList<>();
 
-    public void addVideo(Video video) {
-        videoList.add(video);
+    private FolderParser folderParser;
+
+    public VideoHelper(FolderParser folderParser) {
+        this.folderParser = folderParser;
     }
 
-    public LinkedList<Video> getVideoList() {
-        return videoList;
-    }
-
-    public void setVideoList(LinkedList<Video> videoList) {
-        this.videoList = videoList;
+    public List<Video> getVideoParamFromFiles() throws IOException {
+        List<Video> videoListParam = new ArrayList<>();
+        for (String htmlFilesPath : folderParser.getHTMLFilesPaths()) {
+            HTMLParser htmlParser = new HTMLParser(htmlFilesPath, "UTF-8");
+            String link = htmlParser.getVideoLink();
+            String name = htmlParser.getVideoName();
+            videoListParam.add(new Video(name, link));
+        }
+        return videoListParam;
     }
 }
