2ee4422f7b24e9edab2917fef0d4b16827006097
Roman Chernish <morabatur@gmail.com>
2018-10-14 20:16:10 +0300

    change and rename start method and add startParsingPages

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,9 @@
 import com.chernish.entity.Video;
 import com.chernish.entity.VideoHelper;
 import com.chernish.file.loader.DownloadMaster;
+import com.chernish.file.loader.Saver;
+import com.chernish.parser.FolderParser;
+import com.chernish.parser.HTMLParser;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,13 +18,38 @@ public class Application {
         this.downloadMaster = downloadMaster;
     }
 
+    /**
+     * Метод запускає скачування
+     * @throws IOException
+     */
     private void getMyVideo() throws IOException {
         List<Video> myVideos = videoHelper.getVideoParamFromFiles();
         downloadMaster.downloadVideos(myVideos);
     }
 
-    public void start() throws IOException {
+    /**
+     * Стартує програму
+     * @throws IOException
+     */
+    public void startDownloadVideos() throws IOException {
         getMyVideo();
+    }
+
+    /**
+     * Метод дозволяє отримати список посилань на відео
+     * @param fileParser
+     * @throws IOException
+     */
+    public void startParsingPages(FolderParser fileParser) throws IOException {
+        List<String> listPaths = fileParser.getHTMLFilesPaths();
+        Saver saver = new Saver();
+        for (String path : listPaths) {
+            HTMLParser htmlParser = new HTMLParser(path, "UTF-8");
+            List<String> listLinksFromCoursePage = htmlParser.getListLinksFromCoursePage();
+            String title = htmlParser.getTitlePage();
+            saver.save(listLinksFromCoursePage, title + ".txt");
+        }
+
     }
 
 
